330eb3f551353f464617f6612e4a297d53341bf9
frdpx <155615935+frdpx@users.noreply.github.com>
2024-10-17 08:51:07 +0000

    implement add delete

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,5 @@
 import './Shop.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 function Item(props){
     return (<div key={props.id} onClick={()=>props.callback(props)}>
@@ -10,39 +10,77 @@ function Item(props){
     </div>);
 }
 export default function Shop(){
-        const [products,setProducts]=useState([]);
-        const URL="https://verbose-zebra-6997xj6jw9j7crvqr-5000.app.github.dev";
-        useEffect(()=>{
-            axios.get(URL+'/api/products')
+    const name_ref=useRef(null);
+    const price_ref=useRef(null);
+    const img_ref=useRef(null);
+    const [products,setProducts]=useState([]);
+    const URL ="https://verbose-zebra-6997xj6jw9j7crvqr-5000.app.github.dev";
+    useEffect(()=>{
+        axios.get(URL+'/api/products')
+        .then(response=>{
+            setProducts(response.data);
+        })
+        .catch(error=>{
+            console.log("error");
+        });
+    }
+    ,[]);
+   
+    const [cart,setCart]=useState([]);
+    function addCart(item){
+         setCart([...cart,{id:item.id,name:item.name,price:item.price,img:item.img}]);
+    }
+    const productList=products.map(item=><Item {...item} callback={addCart}/>);
+    const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price}
+        <button onClick={()=>{
+            alert('you click'+index)
+            setCart(cart.filter((i,_index)=>index!=_index));
+            }}>Delete </button>
+        </li>);
+       
+        let totalprice=0;
+        for(let i=0;i<cart.length;i++){
+            totalprice+=cart[i].price;
+        }
+        function addProduct(){
+            const data={
+               name:name_ref.current.value,
+               price:price_ref.current.value,
+               img:img_ref.current.value
+            };
+ 
+            axios.get(URL+'/api/products' ,data)
             .then(response=>{
+                if(response=>{})
+                 setProducts(response.data.products);
+            })
+            .catch(error=>{
+                 console.log("error");
+            });
+        }
+ 
+        function delProduct(id){
+            axios.delete(URL+'/api/products'+id)
+            .then(response=>{
+                if (respond.data.status=="ok") alert("Delete product sucsessfully!");
                 setProducts(response.data);
             })
             .catch(error=>{
                 console.log("error");
             });
         }
-        ,[]);
-        const [cart,setCart]=useState([]);
-        function addCart(item){
-         setCart([...cart,{id:item.id,name:item.name,price:item.price,img:item.img}]);
-        }
-        const productList=products.map(item=><Item {...item} callback={addCart}/>);
-        const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price}
-        <button onClick={()=>{
-            alert('you click'+index);
-            setCart(cart.filter((i,_index)=>index!=_index));
-        }}>
-        Delete</button>
-        </li>);
-        let totalprice=0;
-        for(let i=0;i<cart.length;i++){
-            totalprice+=cart[i].price;
-        }
+       
         return (<>
+        name : <input type="text" ref={name_ref} />
+        price : <input type="text" ref={price_ref}/>
+        img : <input type="text" ref={img_ref}/>
+        <button onClick= { addProduct}>add</button>
+ 
         <div className='grid-container'>{productList}</div>
         <h1>Cart</h1>
-        <button onClick={()=>setCart([])}>Clear all</button>
         <ol>{cartList}</ol>
-        <h2>total price {totalprice} baht</h2>
+        <button onClick={()=>setCart([])}>Clear All</button>
+        <h2>Total Price {totalprice}</h2>
         </>);
 }
+ 
